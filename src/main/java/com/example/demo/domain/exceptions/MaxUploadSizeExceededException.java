0ed073c2f1957980b7df29ea4a29f4eef5b14d11
JiamingSHI <1257198442@qq.com>
2024-03-14 09:16:53 +0100

    Ajustado el tamaño del archivo de avatar subido

--- src/main/java/com/example/demo/domain/exceptions/MaxUploadSizeExceededException.java
+++ src/main/java/com/example/demo/domain/exceptions/MaxUploadSizeExceededException.java
@@ -1,7 +1,7 @@
 package com.example.demo.domain.exceptions;
 
 public class MaxUploadSizeExceededException extends RuntimeException {
-    private static final String DESCRIPTION = "Max Upload Size Exceeded Exception (400)";
+    private static final String DESCRIPTION = "Max Upload Size Exceeded Exception (413)";
 
     public MaxUploadSizeExceededException(String detail) {
         super(DESCRIPTION + ". " + detail);
